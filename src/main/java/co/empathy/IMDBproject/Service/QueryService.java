e81aadd0dcb52baea7b73d4ec37ae1cc4c841145
mariaffnandez <fnandezmaria@gmail.com>
2022-11-09 09:57:58 +0100

    Refactoring queries

--- src/main/java/co/empathy/IMDBproject/Service/QueryService.java
+++ src/main/java/co/empathy/IMDBproject/Service/QueryService.java
@@ -5,7 +5,7 @@ import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.Hit;
 import co.elastic.clients.elasticsearch.core.search.TotalHits;
 import co.empathy.IMDBproject.Model.Movie;
-import org.json.JSONObject;
+
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,67 +20,51 @@ public class QueryService {
         this.client = elasticClient;
     }
 
-    //Multimatch title and synopsis query
-    public List<Movie> multiMatchquery(String indexName, String query) throws IOException {
-        JSONObject jsonObject= new JSONObject(query);
-        String searchText= jsonObject.get("query").toString();
+    //Multimatch query
+    public List<Movie> multiMatchquery(String indexName, String query, List <String> fields) throws IOException {
 
         SearchResponse<Movie> response = client.search(s -> s
                         .index(indexName)
                         .query(q -> q
                                 .multiMatch(t -> t
-                                        .fields("title","synopsis")
-                                        .query(searchText)
+                                        .fields(fields)
+                                        .query(query)
                                 )
                         ),
                 Movie.class
-
         );
-
-        TotalHits total = response.hits().total();
-        List <Movie> filmHits= new ArrayList<Movie>();
-        if (total.value()!=0) {
-            List<Hit<Movie>> hits = response.hits().hits();
-
-            for (Hit<Movie> hit: hits) {
-                Movie movie = hit.source();
-                filmHits.add(movie);
-                System.out.println("Found movie" + movie.getOriginalTitle() + ", score " + hit.score());
-            }
-        } else {
-            System.out.println("There are not results");
-        }
-
-        return filmHits;
+        return hits(response);
 
     }
 
-    //term query
-    public List<Movie> termQuery(String indexName, String query) throws IOException {
-        JSONObject jsonObject= new JSONObject(query);
-        String searchText= jsonObject.get("query").toString();
 
+    //term query
+    public List<Movie> termQuery(String indexName, String query, String field) throws IOException {
         SearchResponse<Movie> response = client.search(s -> s
                         .index(indexName)
                         .query(q -> q
                                 .term(t -> t
-                                        .field("_id")
-                                        .value(searchText)
+                                        .field(field)
+                                        .value(query)
                                 )
                         ),
                 Movie.class
 
         );
 
+        return hits(response);
+
+    }
+    public List<Movie> hits (SearchResponse<Movie> response ){
         TotalHits total = response.hits().total();
-        List <Movie> filmHits= new ArrayList<Movie>();
+        List <Movie> filmHits= new ArrayList<>();
         if (total.value()!=0) {
             List<Hit<Movie>> hits = response.hits().hits();
 
             for (Hit<Movie> hit: hits) {
                 Movie movie = hit.source();
                 filmHits.add(movie);
-                System.out.println("Found movie" + movie.getPrimaryTitle() + ", score " + hit.score());
+                System.out.println("Score "+hit.score());
             }
         } else {
             System.out.println("There are not results");
