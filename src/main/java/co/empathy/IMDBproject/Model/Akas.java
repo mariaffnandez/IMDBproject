46106075fe5e45a5a2b4f8a21f5a6e0ee954350a
mariaffnandez <fnandezmaria@gmail.com>
2022-11-10 09:04:56 +0100

    new akas class

---
+++ src/main/java/co/empathy/IMDBproject/Model/Akas.java
@@ -0,0 +1,16 @@
+package co.empathy.IMDBproject.Model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
+
+public class Akas {
+    String title;
+    String region;
+    String language;
+    Boolean isOriginalTitle;
+
+}
