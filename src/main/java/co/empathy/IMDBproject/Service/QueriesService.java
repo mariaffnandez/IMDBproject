6dd7339bd0db172d1f11db1f22cabf239b55be74
mariaffnandez <fnandezmaria@gmail.com>
2022-11-17 23:12:08 +0100

    Aggregations

--- src/main/java/co/empathy/IMDBproject/Service/QueriesService.java
+++ src/main/java/co/empathy/IMDBproject/Service/QueriesService.java
@@ -1,27 +1,23 @@
 package co.empathy.IMDBproject.Service;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.elasticsearch._types.aggregations.CategorizeTextAggregation;
-import co.elastic.clients.elasticsearch._types.aggregations.HistogramBucket;
+import co.elastic.clients.elasticsearch._types.aggregations.*;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.Hit;
 import co.elastic.clients.elasticsearch.core.search.TotalHits;
+import co.empathy.IMDBproject.Model.Facets.Facets;
+import co.empathy.IMDBproject.Model.Facets.Values;
 import co.empathy.IMDBproject.Model.Filters;
-import co.empathy.IMDBproject.Model.Movie;
-import org.elasticsearch.search.aggregations.Aggregation;
-import org.elasticsearch.search.aggregations.AggregationBuilder;
-import org.elasticsearch.search.aggregations.InternalOrder;
+import co.empathy.IMDBproject.Model.Movie.Movie;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class QueriesService {
     private ElasticsearchClient client;
     private QueryProvider queryProvider;
-    String indexName= "test";
+    String indexName= "imdb";
 
     public QueriesService(ElasticsearchClient elasticClient) {
 
@@ -35,7 +31,9 @@ public class QueriesService {
         SearchResponse response = client.search(s -> s
                         .index(indexName)
                         .query(query)
-                        .size(20),
+
+
+                        ,
                 Movie.class
         );
         return hits(response);
@@ -69,32 +67,36 @@ public class QueriesService {
 
 
         Query query= queryProvider.getFilterQuery(filter);
-
+        System.out.println(query.toString());
         return responseToQuery(query);
 
     }
-    public void aggregationTypeQuery() throws IOException {
-        Query q= queryProvider.rangeQuery(2010,2020,"startYear");
-        String aggregationsName="agg-type";
+    //receive a field that can be titleType or genres and returns a hashmap
+    public Facets aggregationTerms(String field) throws IOException {
+
         SearchResponse<Void> response = client.search(b -> b
                         .index(indexName)
                         .size(0)
-                        .query(q)
-                        .aggregations(aggregationsName, a -> a
-                                .histogram(h -> h
-                                        .field("titleType")
+                        .aggregations(field, a -> a
+                                .terms(h -> h
+                                        .field(field)
                                 )
                         ),
                 Void.class
         );
-        List<HistogramBucket> buckets= response.aggregations()
-                .get(aggregationsName)
-                .histogram().buckets().array();
-        for (HistogramBucket bucket: buckets) {
-           System.out.println("There are " + bucket.docCount() +
-                    "  under " + bucket.key());
+        System.out.println(response.toString());
+        List<StringTermsBucket> buckets = response.aggregations()
+                .get(field)
+                .sterms().buckets().array();
+
+        Map<String, Long> map = new HashMap<>();
+        ArrayList<Values> valuesList= new ArrayList<>();
+        for (StringTermsBucket bucket : buckets) {
+            valuesList.add(new Values(bucket.key().stringValue(),bucket.docCount()));
+
         }
 
+        return new Facets("value","facetGender",valuesList);
 
     }
 
